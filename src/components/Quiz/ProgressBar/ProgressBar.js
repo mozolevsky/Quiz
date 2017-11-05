6c524f6a841445649feec5e98d517cccad676f5e
mozolevsky <alf.rulit@gmail.com>
2017-11-05 17:56:28 +0300

    Progress bar: made mobile version

--- src/components/Quiz/ProgressBar/ProgressBar.js
+++ src/components/Quiz/ProgressBar/ProgressBar.js
@@ -7,15 +7,20 @@ import './ProgressBar.css';
 class ProgressBar extends Component {
     render() {
         const {currentAnswer, totalAnswers, } = this.props;
-        let percent = (currentAnswer / totalAnswers * 100) < 100 ? (currentAnswer / totalAnswers * 100) : 100;
+        let percent = Math.round((currentAnswer / totalAnswers * 100) < 100 ? (currentAnswer / totalAnswers * 100) : 100);
+        let isMobile = document.documentElement.clientWidth < 768;
 
         return (
             <div className="progress-line">
                 <p className="progress-line__desc">{currentAnswer} of {totalAnswers} answered</p>
+                <p className="progress-line__procent-mobile">{percent}%</p>
                 <Line
                     percent={percent}
-                    strokeColor="#7c5cd1"
-                    trailColor="rgba(124, 91, 209, 0.15)"
+                    strokeWidth={isMobile ? 44 : 1}
+                    trailWidth={isMobile ? 44 : 1}
+                    strokeLinecap={isMobile ? 'square' : 'round'}
+                    strokeColor={isMobile ? '#9270ef' : '#7c5cd1'}
+                    trailColor={isMobile ? '#514d55' : "rgba(124, 91, 209, 0.15)"}
                 />
             </div>
         );
